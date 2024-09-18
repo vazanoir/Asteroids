271cd04055eeb0467e0a3e3a08a771be909812f4
Vazanoir <8283348-vazanoir@users.noreply.gitlab.com>
2024-09-18 11:07:16 +0200

    remove old prints

--- main.py
+++ main.py
@@ -5,18 +5,13 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    black = pygame.Color(0, 0, 0)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(black)
+        screen.fill("black")
         pygame.display.flip()
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
